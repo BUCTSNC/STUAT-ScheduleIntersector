8f6c3c55351d1e665b1679d67e3fe8d0c41b5802
快乐的老鼠宝宝 <42690037+LaoshuBaby@users.noreply.github.com>
2021-10-28 20:24:30 +0800

    Update main.py

--- Src/STUAT-SI-GUI/main.py
+++ Src/STUAT-SI-GUI/main.py
@@ -1,8 +1,21 @@
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+# def print_hi(name):
+#     # 在下面的代码行中使用断点来调试脚本。
+#     print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+#
+#
+# # 按间距中的绿色按钮以运行脚本。
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
 
+import uuid
 
-# 按间距中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
+mat = []
+for i in range(5):
+    mat.append([0, 0, 0, 0, 0])
+
+# print(mat)
+
+# 从UUID5生成identicon，用库画出来
+username=input()
+uuid_result=uuid.uuid5(uuid.NAMESPACE_DNS,username)
+print(uuid_result)
